a6297576e263f2eb1b7ff18e3ae3faccf7ac3c8a
Daria <darya.piankova@yandex.ru>
2023-02-04 20:24:05 +0500

    dop task

---
+++ dop.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+string content = File.ReadAllText("input.txt");
+                
+                
+int ScndMax = (int)Char.GetNumericValue(content[0]);
+int firstMax = (int)Char.GetNumericValue(content[0]);
+
+for(int i=0; i<content.Length; i++){
+    if((int)Char.GetNumericValue(content[i])>firstMax){
+        firstMax=(int)Char.GetNumericValue(content[i]);
+    }
+}
+for(int j=0; j<content.Length; j++){
+    if((int)Char.GetNumericValue(content[j])!=firstMax){
+        if((int)Char.GetNumericValue(content[j])>ScndMax){
+        ScndMax=(int)Char.GetNumericValue(content[j]);
+        }
+    }
+}
+StreamWriter sw = new StreamWriter("output.txt");
+sw.WriteLine(ScndMax); 
+sw.Close();
