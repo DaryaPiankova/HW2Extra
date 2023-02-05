a23d32e1bc24eaeb50c4a545248129a41b133a38
Daria <darya.piankova@yandex.ru>
2023-02-05 15:26:32 +0500

    dop task corrected2

--- dop.cs
+++ dop.cs
@@ -4,15 +4,14 @@ int n= Convert.ToInt32(Console.ReadLine());
 int max1=n;
 int max2=n;
 while(n!=0){
-n= Convert.ToInt32(Console.ReadLine());
-if (max2<n && max2<max1){
-    max2=max1;
-}else if(max2>n && max2==max1){
-    max2=n;
-}
-if(max1<n){
-    max1=n; 
-}
-
+    n= Convert.ToInt32(Console.ReadLine());
+    if (max2<n && max2<max1){
+        max2=max1;
+    }else if(max2>n && max2==max1){
+        max2=n;
+    }
+    if(max1<n){
+        max1=n; 
+    }
 }
 Console.WriteLine(max2);
